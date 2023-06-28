22b65fd6e947dc0126089ec96359d3b437facd7e
Justin Pulley <justinpulley@Justins-MacBook-Pro.local>
2023-06-28 17:04:13 -0500

    Chad GPT wrote this, I'm just the messenger, but it def works. I'll understand this later :), right?

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,41 @@ use axum::{
     routing::get,
     Router,
 };
-use ethers::providers::{Http, Middleware, Provider};
+use ethers::providers::{Middleware, Provider};
+use std::sync::Arc;
+use tokio::sync::RwLock;
+use tokio::time::{sleep, Duration};
 
-async fn get_block() -> eyre::Result<String> {
+async fn update_block(block_string: Arc<RwLock<String>>) -> eyre::Result<()> {
     let rpc_url = "https://eth.llamarpc.com";
     let provider = Provider::try_from(rpc_url)?;
-    let block_number = provider.get_block_number().await?;
 
-    Ok(block_number.as_u64().to_string())
+    loop {
+        let block_number = provider.get_block_number().await?;
+
+        {
+            let mut block_str_guard = block_string.write().await;
+            *block_str_guard = block_number.as_u64().to_string();
+        }
+
+        sleep(Duration::from_secs(13)).await;
+    }
 }
 
 #[tokio::main]
 async fn main() {
 
-    let block_string = get_block().await.expect("Failed");
+    let block_string = Arc::new(RwLock::new(String::new()));
 
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { block_string }));
+    tokio::spawn(update_block(block_string.clone()));
+
+    let app = Router::new().route("/", get(move || {
+        let block_str_clone = block_string.clone();
+        async move {
+            let block_str_guard = block_str_clone.read().await;
+            block_str_guard.clone()
+        }
+    }));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
