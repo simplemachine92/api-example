d41dc9f50834c51def8a6a7ad77c331cbd079f61
Justin Pulley <justinpulley@Justins-MacBook-Pro.local>
2023-06-28 16:52:52 -0500

    Basic blockchain example.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@ use axum::{
     routing::get,
     Router,
 };
+use ethers::providers::{Http, Middleware, Provider};
+
+async fn get_block() -> eyre::Result<String> {
+    let rpc_url = "https://eth.llamarpc.com";
+    let provider = Provider::try_from(rpc_url)?;
+    let block_number = provider.get_block_number().await?;
+
+    Ok(block_number.as_u64().to_string())
+}
 
 #[tokio::main]
 async fn main() {
+
+    let block_string = get_block().await.expect("Failed");
+
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!1" }));
+    let app = Router::new().route("/", get(|| async { block_string }));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
